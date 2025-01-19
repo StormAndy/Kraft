ae60cb1dbcfdee093d50a5e9ada4de091a944234
StormAndy <A.bishopart@gmail.com>
2025-01-19 14:03:29 +0000

    Door Interactable
    created first test interactable for Interactable interface for doors
    - supports logic for animator and static graphic state switches
    - supports future extension for Scene transitions

---
+++ Kraft/Assets/Scripts/Actors/Door.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour, IInteractable
+{
+    public bool isOpen = false;
+    public float interactionRange = 3f;  // Default range for door interaction
+
+    public float GetInteractionRange() => interactionRange;  // Return the interaction range for the door
+
+    //Scene Transition?
+    [SerializeField] private bool useAreaTransition = false;
+    [SerializeField] private string sceneName = "ChangeMe";
+
+    //Graphics/Animation Objects
+    [SerializeField] private bool useStaticGraphics = true; //Toggle static graphic states instead of playing an animation
+    [SerializeField] private GameObject staticGraphicClosed, staticGraphicOpen;
+
+    private Animator animator;
+
+    public bool CanInteract(Vector3 interactorPosition)
+    {
+        return Vector3.Distance(interactorPosition, transform.position) <= interactionRange;  // Can interact if within range
+    }
+
+    private void Start()
+    {
+        if (animator == null)
+            animator = this.GetComponent<Animator>();
+    }
+
+    public void Interact()
+    {
+        if (useAreaTransition)
+            Debug.Log("Load Scene: " + sceneName);
+        else
+            ToggleState();
+
+    }
+
+    private void ToggleState()
+    {
+        isOpen = !isOpen;
+        Debug.Log(isOpen ? "Door opened" : "Door closed");  // Toggle door state
+        if (useStaticGraphics && staticGraphicClosed && staticGraphicOpen)
+            if(isOpen)
+            {
+                staticGraphicClosed.SetActive(false);
+                staticGraphicOpen.SetActive(true);
+            }
+            else
+            {
+                staticGraphicClosed.SetActive(true);
+                staticGraphicOpen.SetActive(false);
+            }
+        else
+        {
+            if (animator == null)
+                animator = this.GetComponent<Animator>();
+            if (animator)
+            {
+                if (isOpen)
+                    animator.Play("Open");
+                else
+                    animator.Play("Close");
+            }
+           
+        }
+    }
+
+   
+}
